ee29ebb5bf9535ceda145bfa16be6bd660c77dc4
BrunoRF <brunorf15@gmail.com>
2025-06-14 00:38:39 +0200

    deleted template files

---
+++ src/components/icons/AsciiGeneratorSketch.vue
@@ -0,0 +1,13 @@
+<script setup>
+  
+</script>
+<template>
+  <div>
+    <h1>Ascii Generator Sketch</h1>
+    <p>This component is a placeholder for the Ascii Generator Sketch functionality.</p>
+  </div>
+</template>
+
+<style scoped>
+  /* Your CSS here */
+</style>
