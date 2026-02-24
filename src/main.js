a5fa190b76801654ba4c302aae75e7aef1e88a11
BrunoRF <brunorf15@gmail.com>
2026-02-24 19:00:11 +0100

    implemented pinia store

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import './assets/styles/main.css'
 import { router } from './router'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
 const app = createApp(App)
+const pinia = createPinia()
+
+app.use(pinia)
 app.use(router)
 app.mount('#app')
+
